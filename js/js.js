9d046643d080573543762d9e3c6e32379a8168d8
Carlos <cfernande1408@gmail.com>
2022-11-16 17:34:18 +0100

    creating new branch

--- js/js.js
+++ js/js.js
@@ -24,12 +24,8 @@ let randomNumber = Math.floor(Math.random() * 3);
   }
 }
 
-
-/*const computerSelection = getComputerChoice();*/
-
 function playRound(playerSelection, computerSelection) {
 
-    /*console.log(computerSelection);*/
 
     switch (playerSelection){
 
@@ -103,22 +99,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-        /*switch (juego) {
-
-            case "You have lost!":
-            computerScore++;
-            playedGames++;
-            break;
-
-            case "You have win!":
-            playerScore++;
-            playedGames++;
-            break;
-                
-            case "Tie!":
-            playedGames++;
-            break;
-        }*/
 
     console.log(game());
     console.log(playerScore);
@@ -126,101 +106,4 @@ function playRound(playerSelection, computerSelection) {
     console.log(playedGames);
     console.log(tiedGames);
         
-        
     
-        /*let partida = playRound(playerSelection, computerSelection);
-
-    if (partida == "You have win!"){
-
-        contadorVictorias++;
-
-        resultado = "El resultado del juego º {0} es ....You have win!", i+1 ;
-
-    } else if (partida == "You have lost!"){
-
-        contadorDerrotas++;
-        resultado = "El resultado del juego º {0} es ....You have lost!", i+1;
-
-
-    } else if (partida == "You have lost!")
-
-        resultado = "El resultado del juego º {0} es ....Tie!", i+1;
-
-    }*/
-    
-
-
-
-
-
-
-
-
-/** const computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
-let playerSelection;
-
-let compare = computerSelection.localeCompare(playerSelection);
-
-console.log(compare);
-
-alert("a continuación tendras que seleccionar piedra, papel, tijera");
-
-playerSelection = prompt("¿Qúe deseas jugar?¿Cara quizás?¿Cruz tal vez?¿Papel será?");
-
-function playRound(computerSelection, playerSelection) {
-
-  if (compare != 0) {
-    if (computerSelection == "scissors" && playerSelection == "paper") {
-      return "You have lost!";
-    } else if (computerSelection == "scissors" && playerSelection == "rock") {
-      return "You´ve win!";
-    } else if (computerSelection == "rock" && playerSelection == "scissors") {
-      return "You have lost!";
-    } else if (computerSelection == "rock" && playerSelection == "paper") {
-      return "You´ve win!";
-    } else if (computerSelection == "paper" && playerSelection == "rock") {
-      return "You have lost!";
-    } else if (computerSelection == "paper" && playerSelection == "scissors") {
-      return "You´ve win";
-    }
-  } else {
-    return "Tie!";
-  }
-}
-
-console.log(playRound(computerSelection, playerSelection));
-
-let contadorVictorias = 0;
-
-let contadorDerrotas = 0;
-
-function game() {
-  for (i = 0; i < 5; i++) {
-    let Decision = playRound(computerSelection, playerSelection);
-
-    if (Decision === "You have lost!") {
-      contadorDerrotas++;
-    } else if (Decision === "You´ve win!") {
-      contadorVictorias++;
-    } else {
-      ("Tie! Try again");
-    }
-
-    console.log(i);
-  }
-
-  if (contadorDerrotas > contadorVictorias) {
-    return "You have finally lost";
-  }
-}
-
-game();
-
-console.log(contadorDerrotas);
-
-console.log(contadorVictorias);
-
-*/
